1594991d769f63a46108418d34ae3dd9a3a4c704
Damion Betters <153688977+damionbetters@users.noreply.github.com>
2025-05-26 18:47:42 -0500

    Learning AJAX syntax and changed opacity on a card

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -10,3 +10,42 @@ const countriesContainer = document.querySelector('.countries');
 // https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}
 
 ///////////////////////////////////////
+const getCountryData = function (country) {
+  const request = new XMLHttpRequest();
+  request.open(
+    'GET',
+    `https://restcountries.com/v3.1/name/${country}?fullText=true`
+  );
+  request.send();
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    const html = `
+  <article class="country">
+            <img class="country__img" src="${
+              data.flags?.png ?? 'default.png'
+            }" />
+            <div class="country__data">
+              <h3 class="country__name">${data.name?.common ?? 'no name'}</h3>
+              <h4 class="country__region">${data.region ?? 'no region'}</h4>
+              <p class="country__row"><span>👨🏾‍🤝‍👩🏾</span>${
+                (data.population / 1_000_000).toFixed(1) + ' million'
+              } people</p>
+              <p class="country__row"><span>🗣️</span>${
+                Object.values(data.languages ?? {})[0] ?? 'No official language'
+              }</p> 
+              <p class="country__row"><span>💰</span>${
+                Object.values(data.currencies ?? {})[0]?.name ??
+                'No official currency'
+              }</p>
+            </div>
+          </article>`;
+    // some values are objects. How to get it right.
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+  });
+};
+
+getCountryData('Liberia');
