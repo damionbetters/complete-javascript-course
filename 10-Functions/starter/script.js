c49a5cf4aaf85189b8ea230cab70d9f371673b15
Damion Betters <153688977+damionbetters@users.noreply.github.com>
2025-12-12 07:29:36 -0600

    update js

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,6 @@
 'use strict';
+const announcement =
+  'All passengers come to boarding door 23. Boarding door 23!';
+
+console.log(announcement.replace('door', 'gate'));
+console.log(announcement.replaceAll('door', 'gate'));
